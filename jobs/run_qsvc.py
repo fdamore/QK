018a69bf92c360cfdb30dbdd8a1d69503d3d2326
Francesco D'Amore <francesco.damore@gmail.com>
2024-05-30 10:59:55 +0200

    Added run using QSVC

---
+++ jobs/run_qsvc.py
@@ -0,0 +1,68 @@
+import sys
+import os
+import time
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+from qiskit_machine_learning.kernels import FidelityStatevectorKernel 
+from qiskit.circuit.library import ZZFeatureMap
+from qiskit_machine_learning.algorithms.classifiers import QSVC
+
+import numpy as np
+
+#define working directory and package for QK
+current_wd = os.getcwd()
+sys.path.append(current_wd)
+
+#set the seed
+np.random.seed(123)
+
+#load dataset with panda
+#data are scaled outside the notebook
+env = pd.read_csv('data/env.sel3.scaled.csv')  
+
+
+#DEFINE design matrix
+Y = env['occupancy']
+X = env[['illuminance', 'blinds','lamps','rh', 'co2', 'temp']]
+
+#split design matrix (25% of the design matrix used for test)
+X_train, X_test, y_train, y_test = train_test_split(X, Y)
+
+
+#check the shape of test and training dataset
+print(f'Shape of dataset: {env.shape}')
+print(f'Training shape dataset {X_train.shape}')
+print(f'Label for traing {y_train.shape}')
+
+print(f'Test shape dataset {X_test.shape}')
+print(f'Label for test {y_test.shape}')
+
+#take a feature map and setting QSVC
+NUM_QBIT = X_train.shape[1]
+fm = ZZFeatureMap(feature_dimension=NUM_QBIT)
+q_kernel = FidelityStatevectorKernel(feature_map=fm)
+svm_quantum = QSVC(quantum_kernel=q_kernel)
+
+#show feature map
+print(f'*** FEATURE MAP used in QSVC')
+print(fm.draw())
+
+#get time
+training_start = time.time()
+
+svm_quantum.fit(X_train, y_train)
+
+#get time training
+training_end = time.time()
+
+#result...
+predictions = svm_quantum.predict(X_test)
+score = accuracy_score(predictions, y_test)
+
+#final time (trainign + predict)
+jobs_final_time = time.time()
+
+print(f'*******SCORE: {score}')
+print(f'Time training: {training_end - training_start} seconds. Final time {jobs_final_time - training_start} seconds')
