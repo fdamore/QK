2c677f6c71257de9e6a66c6779e05dcc68e7f902
Francesco D'Amore <francesco.damore@gmail.com>
2024-06-25 19:20:38 +0200

    Adding the plot function to callback

---
+++ jobs/run_adhoc.py
@@ -0,0 +1,122 @@
+import sys
+import os
+import time
+from sklearn.metrics import accuracy_score
+
+#define the trainable kernel
+from qiskit_algorithms.optimizers import SPSA
+from qiskit_machine_learning.utils.loss_functions import SVCLoss
+ 
+from qiskit_machine_learning.algorithms.classifiers import QSVC
+
+from qiskit_machine_learning.kernels.algorithms.quantum_kernel_trainer import QuantumKernelTrainer
+from qiskit_machine_learning.datasets import ad_hoc_data
+from qiskit_algorithms.optimizers import SPSA
+from qiskit_machine_learning.utils.loss_functions import SVCLoss
+from qiskit_algorithms.utils import algorithm_globals
+
+import numpy as np
+
+#define working directory and package for QK
+current_wd = os.getcwd()
+sys.path.append(current_wd)
+
+from qke.QKCallback import QKCallback
+from qke.TrainableCircuits import TrainableCircuits
+from qke.QMeasures import QMeasures
+from qke.TrainableKernelFeatureMap import TrainableKernelFeatureMap
+from qke.CKernels import CKernels
+
+
+
+
+#set the seed(s)
+np.random.seed(123)
+algorithm_globals.random_seed = 123
+
+adhoc_dimension = 3
+X_train, y_train, X_test, y_test, adhoc_total = ad_hoc_data(
+    training_size=30,
+    test_size=5,
+    n=adhoc_dimension,
+    gap=0.3,
+    plot_data=False,
+    one_hot=False,
+    include_sample_total=True,
+)
+
+max_iter = 10
+NUM_QBIT = X_train.shape[1] # nqubit = nuber of dimension (feature)
+
+#check the shape of test> and training dataset
+print(f'Max number of iteration used in kernel optimization: {max_iter}')
+print(f'Training shape dataset {X_train.shape}')
+print(f'Label for traing {y_train.shape}')
+print(f'Test shape dataset {X_test.shape}')
+print(f'Label for test {y_test.shape}')
+print(f'NUM_QUBIT {NUM_QBIT}')
+
+qc_trainable = TrainableCircuits.zzfm(n_wire=NUM_QBIT)
+
+fm = qc_trainable.qc
+training_params = qc_trainable.training_parameters
+
+fm.draw()
+
+#define callback
+my_callback = QKCallback()
+
+
+my_obs = ['ZII', 'IZI','IIZ']
+nshots = 100 #paramenter using primitive estimator
+measure_fn = QMeasures.StateVectorEstimator
+
+#using kernel feature map (change the q_kernel in order to modiy the trainable kernel)
+q_kernel = TrainableKernelFeatureMap(feature_map=fm, training_parameters=training_params)
+q_kernel.configure(obs=my_obs, nshots=nshots, q_measure=QMeasures.StateVectorEstimator, c_kernel=CKernels.rbf)
+
+#q_kernel = TrainableFidelityQuantumKernel(feature_map=fm, training_parameters=training_params)
+
+#define updater, loss and inizial param
+spsa_opt = SPSA(maxiter=max_iter, learning_rate=0.03, perturbation=0.01, termination_checker=my_callback.callback)
+loss_func = SVCLoss(C=1.0)
+#init_point=[np.pi / 2 for _ in range(NUM_QBIT)] 
+init_point = np.random.uniform(size=NUM_QBIT)
+
+print(f'The numbers of shots (if applicable) for (qiskit) primitive estimator: {nshots}')
+print(f'Initial point: {init_point}')
+#print this info
+print(f'The QMeasure function used: {q_kernel.q_measure.__name__}')
+print(f'The classical kernel used: {q_kernel.kernel.__name__}')
+print(f'The observables we use: {my_obs}')
+
+training_kernel_start = time.time()
+
+qk_trainer = QuantumKernelTrainer(quantum_kernel=q_kernel, loss=loss_func, initial_point= init_point, optimizer=spsa_opt)
+qkt_results = qk_trainer.fit(X_train, y_train)
+optimized_kernel = qkt_results.quantum_kernel
+
+training_kernel_end = time.time()
+
+print(f'Time kernel training: {training_kernel_end - training_kernel_start} seconds.')
+
+qsvc = QSVC(quantum_kernel=optimized_kernel)
+
+#get time
+training_svm_start = time.time()
+
+qsvc.fit(X_train, y_train)
+
+#get time training
+training_svm_end = time.time()
+
+#result...
+predictions = qsvc.predict(X_test)
+score = accuracy_score(predictions, y_test)
+
+#final time (trainign + predict)
+jobs_final_time = time.time()
+
+print(f'*******SCORE: {score}')
+print(f'Time training SVM: {training_svm_end - training_svm_start} seconds.')
+print(f'Total jobs time: {jobs_final_time - training_kernel_start} seconds.')
--- qke/QKCallback.py
+++ qke/QKCallback.py
@@ -1,7 +1,17 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
 #calback function
 class QKCallback:
     
-    num_iteration = 0;
+    num_iteration = 0
+   
+
+    def __init__(self) -> None:
+          self._data = [[] for i in range(5)]
 
     #callback function  
     def callback(self, x0, x1=None, x2=None, x3=None, x4=None):
@@ -26,6 +36,23 @@ class QKCallback:
             print(f'Whether the step was accepted: {x4}')
             print(f'**********************')
 
+            self._data[0].append(x0)
+            self._data[1].append(x1)
+            self._data[2].append(x2)
+            self._data[3].append(x3)
+            self._data[4].append(x4)
+
             #return True if you want stop the training
             stop_training = False
             return stop_training
+    
+    def plot_data(self):
+          plt.rcParams["font.size"] = 20
+          #fig, ax = plt.subplots(1, 2, figsize=(14, 5))#plt.subplots(1, 2, figsize=(14, 5))
+          plt.plot([i + 1 for i in range(len(self._data[0]))], np.array(self._data[2]), c="k", marker="o")
+          #ax[0].set_xlabel("Iterations")
+          #ax[0].set_ylabel("Loss")
+          #fig.tight_layout()
+          plt.show()
+    
+
