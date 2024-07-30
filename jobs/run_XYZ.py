d9bd98463f4a00f811a2154fc8ad820b08cc1ced
Francesco D'Amore <francesco.damore@gmail.com>
2024-07-30 16:57:03 +0200

    Traied encode_xyz

---
+++ jobs/run_XYZ.py
@@ -0,0 +1,206 @@
+import sys
+import os
+import time
+import pandas as pd
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+from qiskit_algorithms.utils import algorithm_globals
+
+#define working directory and package for QK
+current_wd = os.getcwd()
+sys.path.append(current_wd)
+
+from qke.CircuitContainer import CircuitContainer
+from qke.Circuits import Circuits
+from qke.QMeasures import QMeasures
+from qke.qkernel import kernel_matrix
+from qke.CKernels import CKernels
+
+
+
+#set the seed
+np.random.seed(123)
+algorithm_globals.random_seed = 123
+
+
+my_obs = ['XXXXXX', 'YYYYYY','ZZZZZZ']
+c = CircuitContainer(qtemplate=Circuits.xyz_encoded, full_ent=False, nwire=6, obs=my_obs, measure_fn=QMeasures.StateVectorEstimator, c_kernel=CKernels.rbf)
+
+
+
+#load dataset with panda
+#data are scaled outside the notebook
+f_rate = 1 #¶ate of data sampling fot testing pourpose
+data_file_csv = 'data/env.sel3.scaled.csv'
+env = pd.read_csv(data_file_csv).sample(frac=f_rate, random_state=123)  
+
+#DEFINE design matrix
+Y = env['occupancy']
+X = env[['illuminance', 'blinds','lamps','rh', 'co2', 'temp']]
+
+#split design matrix (25% of the design matrix used for test)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=123)
+#WARNING: convert data to numpy. Quantum stuff (Qiskit) do not like PANDAS
+X_train_np = X_train.to_numpy()
+y_train_np = y_train.to_numpy()
+
+X_test_np = X_test.to_numpy()
+y_test_np = y_test.to_numpy()
+
+#check the shape of test and training dataset
+print(f'File used for this run: {data_file_csv}')
+print(f'Fraction rate used for this run: {f_rate * 100}%')
+print(f'Shape of dataset: {env.shape}')
+print(f'Training shape dataset {X_train_np.shape}')
+print(f'Label for traing {y_train_np.shape}')
+print(f'Test shape dataset {X_test_np.shape}')
+print(f'Label for test {y_test_np.shape}')
+
+#get time
+t_start = time.time()
+
+svm_quantum = SVC(kernel=kernel_matrix).fit(X_train_np, y_train_np);
+print(f'Sanity check. Dict len after training: {len(c.fm_dict)}')
+
+#get time training
+t_training = time.time()
+
+#result...
+predictions = svm_quantum.predict(X_test_np)
+score = accuracy_score(predictions, y_test)
+
+#final time (trainign + predict)
+t_final = time.time()
+
+print(f'*******SCORE: {score}')
+print(f'Time training: {t_training - t_start} seconds. Final time {t_final - t_start} seconds')
+print(f'Sanity check. Dict len after prediction: {len(c.fm_dict)}')
+
+c.save_feature_map(prefix='run_xyz_')
+
+# *** Create a Container ***
+# *** Quantum template for feature map using 6 qubit ***
+#      ┌───────────┐┌───────────┐┌───────────┐                         ┌───┐
+# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├──■──────────────────────┤ X ├
+#      ├───────────┤├───────────┤├───────────┤┌─┴─┐                    └─┬─┘
+# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├┤ X ├──■───────────────────┼──
+#      ├───────────┤├───────────┤├───────────┤└───┘┌─┴─┐                 │  
+# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├─────┤ X ├──■──────────────┼──
+#      ├───────────┤├───────────┤├───────────┤     └───┘┌─┴─┐            │  
+# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├──────────┤ X ├──■─────────┼──
+#      ├───────────┤├───────────┤├───────────┤          └───┘┌─┴─┐       │  
+# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├───────────────┤ X ├──■────┼──
+#      ├───────────┤├───────────┤├───────────┤               └───┘┌─┴─┐  │  
+# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├────────────────────┤ X ├──■──
+#      └───────────┘└───────────┘└───────────┘                    └───┘     
+# *** Required observables: ['XXXXXX', 'YYYYYY', 'ZZZZZZ']
+# *** Measure procedure: StateVectorEstimator
+# *** CKernel function used: rbf
+# File used for this run: data/env.sel3.minmax.csv
+# File used for this run: data/env.sel3.minmax.csv
+# Fraction rate used for this run: 100%
+# Shape of dataset: (2865, 7)
+# Training shape dataset (2148, 6)
+# Label for traing (2148,)
+# Test shape dataset (717, 6)
+# Label for test (717,)
+# Sanity check. Dict len after training: 2148
+# *******SCORE: 0.5718270571827058
+# Time training: 354.1382703781128 seconds. Final time 476.2751247882843 seconds
+# Sanity check. Dict len after prediction: 2865
+# Timestamp of the file storing data: 20240730155639
+
+# *** Create a Container ***
+# *** Quantum template for feature map using 6 qubit ***
+#      ┌───────────┐┌───────────┐┌───────────┐
+# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├
+#      └───────────┘└───────────┘└───────────┘
+# *** Required observables: ['XXXXXX', 'YYYYYY', 'ZZZZZZ']
+# *** Measure procedure: StateVectorEstimator
+# *** CKernel function used: rbf
+# File used for this run: data/env.sel3.minmax.csv
+# Fraction rate used for this run: 100%
+# Shape of dataset: (2865, 7)
+# Training shape dataset (2148, 6)
+# Label for traing (2148,)
+# Test shape dataset (717, 6)
+# Label for test (717,)
+# Sanity check. Dict len after training: 2148
+# *******SCORE: 0.5718270571827058
+# Time training: 354.6549348831177 seconds. Final time 472.41593194007874 seconds
+# Sanity check. Dict len after prediction: 2865
+# Timestamp of the file storing data: 20240730161736
+
+# *** Create a Container ***
+# *** Quantum template for feature map using 6 qubit ***
+#      ┌───────────┐┌───────────┐┌───────────┐                         ┌───┐
+# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├──■──────────────────────┤ X ├
+#      ├───────────┤├───────────┤├───────────┤┌─┴─┐                    └─┬─┘
+# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├┤ X ├──■───────────────────┼──
+#      ├───────────┤├───────────┤├───────────┤└───┘┌─┴─┐                 │  
+# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├─────┤ X ├──■──────────────┼──
+#      ├───────────┤├───────────┤├───────────┤     └───┘┌─┴─┐            │  
+# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├──────────┤ X ├──■─────────┼──
+#      ├───────────┤├───────────┤├───────────┤          └───┘┌─┴─┐       │  
+# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├───────────────┤ X ├──■────┼──
+#      ├───────────┤├───────────┤├───────────┤               └───┘┌─┴─┐  │  
+# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├────────────────────┤ X ├──■──
+#      └───────────┘└───────────┘└───────────┘                    └───┘     
+# *** Required observables: ['XXXXXX', 'YYYYYY', 'ZZZZZZ']
+# *** Measure procedure: StateVectorEstimator
+# *** CKernel function used: rbf
+# File used for this run: data/env.sel3.scaled.csv
+# Fraction rate used for this run: 100%
+# Shape of dataset: (2865, 7)
+# Training shape dataset (2148, 6)
+# Label for traing (2148,)
+# Test shape dataset (717, 6)
+# Label for test (717,)
+# Sanity check. Dict len after training: 2148
+# *******SCORE: 0.5718270571827058
+# Time training: 350.6267855167389 seconds. Final time 467.2365198135376 seconds
+# Sanity check. Dict len after prediction: 2865
+# Timestamp of the file storing data: 20240730162837
+
+# *** Create a Container ***
+# *** Quantum template for feature map using 6 qubit ***
+#      ┌───────────┐┌───────────┐┌───────────┐
+# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├
+#      └───────────┘└───────────┘└───────────┘
+# *** Required observables: ['XXXXXX', 'YYYYYY', 'ZZZZZZ']
+# *** Measure procedure: StateVectorEstimator
+# *** CKernel function used: rbf
+# File used for this run: data/env.sel3.scaled.csv
+# Fraction rate used for this run: 100%
+# Shape of dataset: (2865, 7)
+# Training shape dataset (2148, 6)
+# Label for traing (2148,)
+# Test shape dataset (717, 6)
+# Label for test (717,)
+# Sanity check. Dict len after training: 2148
+# *******SCORE: 0.5718270571827058
+# Time training: 343.1508548259735 seconds. Final time 461.1181118488312 seconds
+# Sanity check. Dict len after prediction: 2865
+# Timestamp of the file storing data: 20240730164247
