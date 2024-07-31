91533b10b3a107e6bc4817887d383c55ecd2b11f
Francesco D'Amore <francesco.damore@gmail.com>
2024-07-31 16:42:08 +0200

    Complete the run for the summer 2024 submission

--- jobs/run_XYZ.py
+++ jobs/run_XYZ.py
@@ -25,8 +25,10 @@ algorithm_globals.random_seed = 123
 
 
 #my_obs = ['XIIIII', 'IXIIII','IIXIII', 'IIIXII','IIIIXI','IIIIIX','YIIIII', 'IYIIII','IIYIII', 'IIIYII','IIIIYI','IIIIIY','ZIIIII', 'IZIIII','IIZIII', 'IIIZII','IIIIZI','IIIIIZ']
-my_obs = ['ZIIIII', 'IZIIII','IIZIII', 'IIIZII','IIIIZI','IIIIIZ']
-c = CircuitContainer(qtemplate=Circuits.xyz_encoded, full_ent=True, nwire=6, obs=my_obs, measure_fn=QMeasures.StateVectorEstimator, c_kernel=CKernels.rbf)
+#my_obs = ['XIIIII', 'IXIIII','IIXIII', 'IIIXII','IIIIXI','IIIIIX']
+my_obs = ['YIIIII', 'IYIIII','IIYIII', 'IIIYII','IIIIYI','IIIIIY']
+#my_obs = ['ZIIIII', 'IZIIII','IIZIII', 'IIIZII','IIIIZI','IIIIIZ']
+c = CircuitContainer(qtemplate=Circuits.xyz_encoded, full_ent=False, nwire=6, obs=my_obs, measure_fn=QMeasures.StateVectorEstimator, c_kernel=CKernels.rbf)
 
 
 
@@ -95,6 +97,101 @@ c.save_feature_map(prefix='run_xyz_')
 #      ├───────────┤├───────────┤├───────────┤               └───┘┌─┴─┐  │  
 # q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├────────────────────┤ X ├──■──
 #      └───────────┘└───────────┘└───────────┘                    └───┘     
+# *** Required observables: ['XIIIII', 'IXIIII', 'IIXIII', 'IIIXII', 'IIIIXI', 'IIIIIX', 'YIIIII', 'IYIIII', 'IIYIII', 'IIIYII', 'IIIIYI', 'IIIIIY', 'ZIIIII', 'IZIIII', 'IIZIII', 'IIIZII', 'IIIIZI', 'IIIIIZ']
+# *** Measure procedure: StateVectorEstimator
+# *** CKernel function used: rbf
+# File used for this run: data/env.sel3.scaled.csv
+# Fraction rate used for this run: 100%
+# Shape of dataset: (2865, 7)
+# Training shape dataset (2148, 6)
+# Label for traing (2148,)
+# Test shape dataset (717, 6)
+# Label for test (717,)
+# Sanity check. Dict len after training: 2148
+# *******SCORE: 0.7796373779637378
+# Time training: 290.09974360466003 seconds. Final time 384.48747658729553 seconds
+# Sanity check. Dict len after prediction: 2865
+# Timestamp of the file storing data: 20240730220334
+
+# *** Create a Container ***
+# *** Quantum template for feature map using 6 qubit ***
+#      ┌───────────┐┌───────────┐┌───────────┐                         ┌───┐
+# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├──■──────────────────────┤ X ├
+#      ├───────────┤├───────────┤├───────────┤┌─┴─┐                    └─┬─┘
+# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├┤ X ├──■───────────────────┼──
+#      ├───────────┤├───────────┤├───────────┤└───┘┌─┴─┐                 │  
+# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├─────┤ X ├──■──────────────┼──
+#      ├───────────┤├───────────┤├───────────┤     └───┘┌─┴─┐            │  
+# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├──────────┤ X ├──■─────────┼──
+#      ├───────────┤├───────────┤├───────────┤          └───┘┌─┴─┐       │  
+# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├───────────────┤ X ├──■────┼──
+#      ├───────────┤├───────────┤├───────────┤               └───┘┌─┴─┐  │  
+# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├────────────────────┤ X ├──■──
+#      └───────────┘└───────────┘└───────────┘                    └───┘     
+# *** Required observables: ['XIIIII', 'IXIIII', 'IIXIII', 'IIIXII', 'IIIIXI', 'IIIIIX']
+# *** Measure procedure: StateVectorEstimator
+# *** CKernel function used: rbf
+# File used for this run: data/env.sel3.scaled.csv
+# Fraction rate used for this run: 100%
+# Shape of dataset: (2865, 7)
+# Training shape dataset (2148, 6)
+# Label for traing (2148,)
+# Test shape dataset (717, 6)
+# Label for test (717,)
+# Sanity check. Dict len after training: 2148
+# *******SCORE: 0.7782426778242678
+# Time training: 289.31462812423706 seconds. Final time 380.59103655815125 seconds
+# Sanity check. Dict len after prediction: 2865
+# Timestamp of the file storing data: 20240731154606
+
+# *** Create a Container ***
+# *** Quantum template for feature map using 6 qubit ***
+#      ┌───────────┐┌───────────┐┌───────────┐                         ┌───┐
+# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├──■──────────────────────┤ X ├
+#      ├───────────┤├───────────┤├───────────┤┌─┴─┐                    └─┬─┘
+# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├┤ X ├──■───────────────────┼──
+#      ├───────────┤├───────────┤├───────────┤└───┘┌─┴─┐                 │  
+# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├─────┤ X ├──■──────────────┼──
+#      ├───────────┤├───────────┤├───────────┤     └───┘┌─┴─┐            │  
+# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├──────────┤ X ├──■─────────┼──
+#      ├───────────┤├───────────┤├───────────┤          └───┘┌─┴─┐       │  
+# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├───────────────┤ X ├──■────┼──
+#      ├───────────┤├───────────┤├───────────┤               └───┘┌─┴─┐  │  
+# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├────────────────────┤ X ├──■──
+#      └───────────┘└───────────┘└───────────┘                    └───┘     
+# *** Required observables: ['YIIIII', 'IYIIII', 'IIYIII', 'IIIYII', 'IIIIYI', 'IIIIIY']
+# *** Measure procedure: StateVectorEstimator
+# *** CKernel function used: rbf
+# File used for this run: data/env.sel3.scaled.csv
+# Fraction rate used for this run: 100%
+# Shape of dataset: (2865, 7)
+# Training shape dataset (2148, 6)
+# Label for traing (2148,)
+# Test shape dataset (717, 6)
+# Label for test (717,)
+# Sanity check. Dict len after training: 2148
+# *******SCORE: 0.7377963737796374
+# Time training: 284.4429986476898 seconds. Final time 373.8829274177551 seconds
+# Sanity check. Dict len after prediction: 2865
+# Timestamp of the file storing data: 20240731160217
+
+
+
+# *** Create a Container *** RUN ON SERVER
+# *** Quantum template for feature map using 6 qubit ***
+#      ┌───────────┐┌───────────┐┌───────────┐                         ┌───┐
+# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├──■──────────────────────┤ X ├
+#      ├───────────┤├───────────┤├───────────┤┌─┴─┐                    └─┬─┘
+# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├┤ X ├──■───────────────────┼──
+#      ├───────────┤├───────────┤├───────────┤└───┘┌─┴─┐                 │  
+# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├─────┤ X ├──■──────────────┼──
+#      ├───────────┤├───────────┤├───────────┤     └───┘┌─┴─┐            │  
+# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├──────────┤ X ├──■─────────┼──
+#      ├───────────┤├───────────┤├───────────┤          └───┘┌─┴─┐       │  
+# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├───────────────┤ X ├──■────┼──
+#      ├───────────┤├───────────┤├───────────┤               └───┘┌─┴─┐  │  
+# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├────────────────────┤ X ├──■──
+#      └───────────┘└───────────┘└───────────┘                    └───┘     
 # *** Required observables: ['ZIIIII', 'IZIIII', 'IIZIII', 'IIIZII', 'IIIIZI', 'IIIIIZ']
 # *** Measure procedure: StateVectorEstimator
 # *** CKernel function used: rbf
@@ -110,37 +207,6 @@ c.save_feature_map(prefix='run_xyz_')
 # Time training: 287.47757482528687 seconds. Final time 378.4391973018646 seconds
 # Sanity check. Dict len after prediction: 2865
 # Timestamp of the file storing data: 20240730223027
-
-# *** Create a Container *** RUN ON SERVER
-# *** Quantum template for feature map using 6 qubit ***
-#      ┌───────────┐┌───────────┐┌───────────┐
-# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├
-#      ├───────────┤├───────────┤├───────────┤
-# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├
-#      ├───────────┤├───────────┤├───────────┤
-# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├
-#      ├───────────┤├───────────┤├───────────┤
-# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├
-#      ├───────────┤├───────────┤├───────────┤
-# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├
-#      ├───────────┤├───────────┤├───────────┤
-# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├
-#      └───────────┘└───────────┘└───────────┘
-# *** Required observables: ['ZIIIII', 'IZIIII', 'IIZIII', 'IIIZII', 'IIIIZI', 'IIIIIZ']
-# *** Measure procedure: StateVectorEstimator
-# *** CKernel function used: rbf
-# File used for this run: data/env.sel3.scaled.csv
-# Fraction rate used for this run: 100%
-# Shape of dataset: (2865, 7)
-# Training shape dataset (2148, 6)
-# Label for traing (2148,)
-# Test shape dataset (717, 6)
-# Label for test (717,)
-# Sanity check. Dict len after training: 2148
-# *******SCORE: 0.7810320781032078
-# Time training: 285.8408553600311 seconds. Final time 375.35733103752136 seconds
-# Sanity check. Dict len after prediction: 2865
-# Timestamp of the file storing data: 20240730222308
 
 # *** Create a Container *** RUN ON SERVER
 # *** Quantum template for feature map using 6 qubit ***
@@ -173,23 +239,22 @@ c.save_feature_map(prefix='run_xyz_')
 # Sanity check. Dict len after prediction: 2865
 # Timestamp of the file storing data: 20240730221306
 
-
-# *** Create a Container *** RUN ON SERVER
+# *** Create a Container ***
 # *** Quantum template for feature map using 6 qubit ***
-#      ┌───────────┐┌───────────┐┌───────────┐                         ┌───┐
-# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├──■──────────────────────┤ X ├
-#      ├───────────┤├───────────┤├───────────┤┌─┴─┐                    └─┬─┘
-# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├┤ X ├──■───────────────────┼──
-#      ├───────────┤├───────────┤├───────────┤└───┘┌─┴─┐                 │  
-# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├─────┤ X ├──■──────────────┼──
-#      ├───────────┤├───────────┤├───────────┤     └───┘┌─┴─┐            │  
-# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├──────────┤ X ├──■─────────┼──
-#      ├───────────┤├───────────┤├───────────┤          └───┘┌─┴─┐       │  
-# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├───────────────┤ X ├──■────┼──
-#      ├───────────┤├───────────┤├───────────┤               └───┘┌─┴─┐  │  
-# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├────────────────────┤ X ├──■──
-#      └───────────┘└───────────┘└───────────┘                    └───┘     
-# *** Required observables: ['XIIIII', 'IXIIII', 'IIXIII', 'IIIXII', 'IIIIXI', 'IIIIIX', 'YIIIII', 'IYIIII', 'IIYIII', 'IIIYII', 'IIIIYI', 'IIIIIY', 'ZIIIII', 'IZIIII', 'IIZIII', 'IIIZII', 'IIIIZI', 'IIIIIZ']
+#      ┌───────────┐┌───────────┐┌───────────┐
+# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├
+#      └───────────┘└───────────┘└───────────┘
+# *** Required observables: ['XIIIII', 'IXIIII', 'IIXIII', 'IIIXII', 'IIIIXI', 'IIIIIX']
 # *** Measure procedure: StateVectorEstimator
 # *** CKernel function used: rbf
 # File used for this run: data/env.sel3.scaled.csv
@@ -200,7 +265,73 @@ c.save_feature_map(prefix='run_xyz_')
 # Test shape dataset (717, 6)
 # Label for test (717,)
 # Sanity check. Dict len after training: 2148
-# *******SCORE: 0.7796373779637378
-# Time training: 290.09974360466003 seconds. Final time 384.48747658729553 seconds
+# *******SCORE: 0.7949790794979079
+# Time training: 279.3285255432129 seconds. Final time 367.8740088939667 seconds
 # Sanity check. Dict len after prediction: 2865
-# Timestamp of the file storing data: 20240730220334
+# Timestamp of the file storing data: 20240731161722
+
+# *** Create a Container ***
+# *** Quantum template for feature map using 6 qubit ***
+#      ┌───────────┐┌───────────┐┌───────────┐
+# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├
+#      └───────────┘└───────────┘└───────────┘
+# *** Required observables: ['YIIIII', 'IYIIII', 'IIYIII', 'IIIYII', 'IIIIYI', 'IIIIIY']
+# *** Measure procedure: StateVectorEstimator
+# *** CKernel function used: rbf
+# File used for this run: data/env.sel3.scaled.csv
+# Fraction rate used for this run: 100%
+# Shape of dataset: (2865, 7)
+# Training shape dataset (2148, 6)
+# Label for traing (2148,)
+# Test shape dataset (717, 6)
+# Label for test (717,)
+# Sanity check. Dict len after training: 2148
+# *******SCORE: 0.7629009762900977
+# Time training: 282.0942256450653 seconds. Final time 371.38722467422485 seconds
+# Sanity check. Dict len after prediction: 2865
+# Timestamp of the file storing data: 20240731163231
+
+# *** Create a Container *** RUN ON SERVER
+# *** Quantum template for feature map using 6 qubit ***
+#      ┌───────────┐┌───────────┐┌───────────┐
+# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├
+#      └───────────┘└───────────┘└───────────┘
+# *** Required observables: ['ZIIIII', 'IZIIII', 'IIZIII', 'IIIZII', 'IIIIZI', 'IIIIIZ']
+# *** Measure procedure: StateVectorEstimator
+# *** CKernel function used: rbf
+# File used for this run: data/env.sel3.scaled.csv
+# Fraction rate used for this run: 100%
+# Shape of dataset: (2865, 7)
+# Training shape dataset (2148, 6)
+# Label for traing (2148,)
+# Test shape dataset (717, 6)
+# Label for test (717,)
+# Sanity check. Dict len after training: 2148
+# *******SCORE: 0.7810320781032078
+# Time training: 285.8408553600311 seconds. Final time 375.35733103752136 seconds
+# Sanity check. Dict len after prediction: 2865
+# Timestamp of the file storing data: 20240730222308
+
+
+
+
