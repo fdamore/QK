560e06913deaad8772684d9454cb5b757e07a5c0
Francesco D'Amore <francesco.damore@gmail.com>
2024-10-17 17:58:19 +0200

    Improve grid search

--- jobs/run_XYZ_PQK_gridsearch.py
+++ jobs/run_XYZ_PQK_gridsearch.py
@@ -2,6 +2,8 @@ import sys
 import os
 import time
 import pandas as pd
+from sklearn.base import accuracy_score
+from sklearn.metrics import classification_report
 from sklearn.model_selection import GridSearchCV, train_test_split
 import numpy as np
 from qiskit_algorithms.utils import algorithm_globals
@@ -35,7 +37,7 @@ pqk.metadata()
 
 #load dataset with panda
 #data are scaled outside the notebook
-f_rate = 0.1 #rate of data sampling fot testing pourpose (GRID SEARCH RATE)
+f_rate = 1 #rate of data sampling fot testing pourpose 
 data_file_csv = 'data/env.sel3.scaled.csv'
 env = pd.read_csv(data_file_csv).sample(frac=f_rate, random_state=123)  
 
@@ -48,41 +50,53 @@ X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=123)
 #WARNING: convert data to numpy. Quantum stuff (Qiskit) do not like PANDAS
 X_train_np = X_train.to_numpy()
 y_train_np = y_train.to_numpy()
-
+#LAST RESULT:
+##USING 1000 data point for training
+##*******SCORE: 0.8222384784198976
 X_test_np = X_test.to_numpy()
 y_test_np = y_test.to_numpy()
 
 #check the shape of test and training dataset
-print(f'File used for this run: {data_file_csv}')
-print(f'Fraction rate used for this run: {f_rate * 100}%')
 print(f'Shape of dataset: {env.shape}')
 print(f'Training shape dataset {X_train_np.shape}')
 print(f'Label for traing {y_train_np.shape}')
+
 print(f'Test shape dataset {X_test_np.shape}')
 print(f'Label for test {y_test_np.shape}')
 
 # define grid search strategy
 #Create a dictionary of possible parameters
 params_grid = {'C': [0.006, 0.015, 0.03, 0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256, 512, 1024],
-          'gamma': np.array([0.10, 0.15, 0.25, 0.5, 0.75, 1.0, 1.25,1.50, 1.75, 2.0, 2.5, 3.0, 4.0])}
+          'gamma': np.array([0.10, 0.15, 0.25, 0.5, 0.75, 1.0, 1.25,1.50, 1.75, 2.0, 2.5, 3.0,3.5,3.7, 4.0]),          
+          'kernel':['rbf'] }
 
 
 
-#Create the GridSearchCV object
-grid_clf = GridSearchCV(pqk, params_grid, verbose=4)
+#Create the GridSearchCV object (be carefull... it uses all processor on the host machine)
+grid = GridSearchCV(pqk, params_grid, verbose=1, n_jobs=-1)
 
 #get time
 t_start = time.time()
 
 
 #Fit the data with the best possible parameters
-grid_clf = grid_clf.fit(X_train_np, y_train_np)
+grid_clf = grid.fit(X_train_np, y_train_np)
 
 #get time training
 t_training = time.time()
 
 #Print the best estimator with it's parameters
-print(f'Best paramenter: {grid_clf.best_params_}')
+print(f'Best paramenter: {grid.best_params_}')
+
+#perform grid prediction on test set
+grid_predictions = grid.predict(X_test) 
+
+# print classification report 
+print(classification_report(y_test, grid_predictions))
+
+#print scro a comparison
+score = accuracy_score(grid_predictions, y_test)
+print(f'Accuracy Score on data: {score}')
 
 #GRID SEARCH HIGH VERBOSITY
 # [CV 4/5] END .................C=0.25, gamma=4.0;, score=0.767 total time=   2.0s
