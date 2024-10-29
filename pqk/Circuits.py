5975ccf50491903383f7c92753146e7e6de8d9d0
Francesco D'Amore <francesco.damore@gmail.com>
2024-10-29 16:18:39 +0100

    Added prefix name to Circuits.

--- pqk/Circuits.py
+++ pqk/Circuits.py
@@ -1,3 +1,4 @@
+from textwrap import wrap
 from qiskit.circuit import Parameter
 from qiskit.circuit.library import ZZFeatureMap
 from qiskit.circuit.library import IQP
@@ -8,32 +9,28 @@ import numpy as np
 
 class Circuits:
 
+    
     @staticmethod
-    def iqpfeaturemap(n_wire, full_ent = True):
-        mat_param = np.real(random_hermitian(n_wire, seed=1234))
-        circuit = IQP(mat_param)
-        return circuit   
+    def zzfeaturemap(n_wire, full_ent = True, param_prefix = 'phi'):
+        zfm = ZZFeatureMap(feature_dimension=n_wire, parameter_prefix=param_prefix)
 
-
-    @staticmethod
-    def zzfeaturemap(n_wire, full_ent = True):
-        zfm = ZZFeatureMap(feature_dimension=n_wire)
         return zfm   
 
 
     #cascade embedding
     @staticmethod
-    def cascade(n_wire, full_ent = True):
+    def cascade(n_wire, full_ent = True, param_prefix = 'phi'):
 
         # Create a new circuit with two qubits
         qc = QuantumCircuit(n_wire)
+        qc.name = 'Cascade'
         
         for i in range(n_wire):
 
             #add hadamrd
             qc.h(i)
 
-            phi_name = 'phi_'+str(i)
+            phi_name = param_prefix + '_'+str(i)
             phi = Parameter(phi_name)
             qc.rx(phi, i)
 
@@ -47,10 +44,11 @@ class Circuits:
 
     #embedding - encoded used in paper Hubregsten et all.
     @staticmethod
-    def ansatz_encoded(n_wire, full_ent = True):
+    def ansatz_encoded(n_wire, full_ent = True, param_prefix = 'phi'):
 
         # Create a new circuit with two qubits
         qc = QuantumCircuit(n_wire)
+        qc.name = 'AnsatzEncoded'
         
         for i in range(n_wire):
                     
@@ -59,7 +57,7 @@ class Circuits:
 
         for i in range(n_wire):
             
-            phi_name = 'phi_'+str(i)
+            phi_name = param_prefix + '_' + str(i)
             phi = Parameter(phi_name)
             qc.rz(phi, i)
 
@@ -78,14 +76,15 @@ class Circuits:
         return qc
     
     @staticmethod
-    def xyz_encoded(n_wire, full_ent = True):
+    def xyz_encoded(n_wire, full_ent = True, param_prefix = 'phi'):
 
         # Create a new circuit with two qubits
-        qc = QuantumCircuit(n_wire)        
+        qc = QuantumCircuit(n_wire) 
+        qc.name = 'XYZ'       
         
 
         for i in range(n_wire):            
-            phi_name = 'phi_'+str(i)
+            phi_name = param_prefix + '_' + str(i)
             phi = Parameter(phi_name)
             qc.rx(phi, i) 
             qc.ry(phi, i)
@@ -99,14 +98,15 @@ class Circuits:
         return qc 
     
     @staticmethod
-    def spiral_encoding(n_wire, n_windings, full_ent = True):   # im considering n_wire as number of qubits (not features) - Luca
+    def spiral_encoding(n_wire, n_windings, full_ent = True, param_prefix = 'phi'):   # im considering n_wire as number of qubits (not features) - Luca
 
         # Create a new circuit with two qubits
-        qc = QuantumCircuit(n_wire)        
+        qc = QuantumCircuit(n_wire)
+        qc.name = 'Spiral'        
         
         # assuming phis normalized bw 0 and 1
         for i in range(n_wire):            
-            phi_name = 'phi_'+str(i)
+            phi_name = param_prefix + '_' + str(i)
             phi = Parameter(phi_name)
             qc.ry(np.pi * phi, i)
             qc.rz(2 * np.pi * n_windings * phi, i)    # n_windings is the number of times the spiral makes a complete turn - Luca
@@ -119,16 +119,17 @@ class Circuits:
         return qc 
 
     @staticmethod
-    def uniform_bloch_encoding(n_wire, full_ent = True):   # requires half as much qubits like dense encoding! - L
+    def uniform_bloch_encoding(n_wire, full_ent = True, param_prefix = 'phi'):   # requires half as much qubits like dense encoding! - L
 
         # Create a new circuit with two qubits
-        qc = QuantumCircuit(n_wire)        
+        qc = QuantumCircuit(n_wire)
+        qc.name = 'UniformBloch'        
         
         # assuming phis, thetas normalized bw 0 and 1
         for i in range(n_wire):            
-            theta_name = 'phi_'+str(2*i)
+            theta_name = param_prefix + '_'+str(2*i)
             theta = Parameter(theta_name)
-            phi_name = 'phi_'+str(2*i+1)
+            phi_name = param_prefix + '_'+str(2*i+1)
             phi = Parameter(phi_name)
             qc.ry(2*np.arccos(np.sqrt(theta)), i)
             qc.rz(phi*np.pi, i)
@@ -138,17 +139,17 @@ class Circuits:
                 qc.cx(i%n_wire, (i+1)%n_wire)
         
         return qc 
-
-
+    
     @staticmethod
-    def x_encoded(n_wire, full_ent = True):
+    def x_encoded(n_wire, full_ent = True, param_prefix = 'phi'):
 
         # Create a new circuit with two qubits
-        qc = QuantumCircuit(n_wire)        
+        qc = QuantumCircuit(n_wire) 
+        qc.name = 'XEncoded'       
         
 
         for i in range(n_wire):            
-            phi_name = 'phi_'+str(i)
+            phi_name = param_prefix + '_'+str(i)
             phi = Parameter(phi_name)
             qc.rx(phi, i)      
 
@@ -160,62 +161,8 @@ class Circuits:
         return qc 
 
 
-    @staticmethod
-    def y_encoded(n_wire, full_ent = True):
 
-        # Create a new circuit with two qubits
-        qc = QuantumCircuit(n_wire)        
-        
-
-        for i in range(n_wire):            
-            phi_name = 'phi_'+str(i)
-            phi = Parameter(phi_name)
-            qc.ry(phi, i)
-
-        if(full_ent):
-            for i in range(n_wire):            
-                qc.cx(i%n_wire, (i+1)%n_wire)        
-      
-        
-        return qc
-
-
-    @staticmethod
-    def y_encoded_scaled(n_wire, full_ent = True):
-
-        # Create a new circuit with two qubits
-        qc = QuantumCircuit(n_wire)               
-
-        for i in range(n_wire):
-            
-            phi_name = 'phi_'+str(i)
-            phi = Parameter(phi_name)
-            qc.ry(((phi + 1)/2) * np.pi, i)
-
-        if(full_ent):
-            for i in range(n_wire):
-                qc.cx(i%n_wire, (i+1)%n_wire)        
-      
-        
-        return qc
+  
 
 
 
-
-    @staticmethod
-    def z_encoded(n_wire, full_ent = True):
-
-        # Create a new circuit with two qubits
-        qc = QuantumCircuit(n_wire)   
-        
-
-        for i in range(n_wire):            
-            phi_name = 'phi_'+str(i)
-            phi = Parameter(phi_name)
-            qc.rz(phi, i) 
-
-        if(full_ent):
-            for i in range(n_wire):            
-                qc.cx(i%n_wire, (i+1)%n_wire)       
-        
-        return qc
