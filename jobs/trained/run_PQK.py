4a0ecb65aee22952cf8cc5ac8675af03619e46f8
Francesco D'Amore <francesco.damore@gmail.com>
2024-12-06 17:46:54 +0100

    Improve run script for tranable version

--- jobs/trained/run_PQK.py
+++ jobs/trained/run_PQK.py
@@ -1,8 +1,5 @@
-
 import sys
 import os
-
-from pqk import Circuits
 
 #define working directory and package for QK
 current_wd = os.getcwd()
@@ -27,24 +24,32 @@ from pqk.CKernels import CKernels
 from pqk.Circuits import Circuits
 from qiskit_algorithms.utils import algorithm_globals
 
+
+
+#set the seed
+seed=123
+np.random.seed(seed)
+algorithm_globals.random_seed = seed
+
+
+my_obs = ['XIIIII', 'IXIIII','IIXIII', 'IIIXII','IIIIXI','IIIIIX','YIIIII', 'IYIIII','IIYIII', 'IIIYII','IIIIYI','IIIIIY','ZIIIII', 'IZIIII','IIZIII', 'IIIZII','IIIIZI','IIIIIZ']
+#my_obs = ['XIIIII', 'IXIIII','IIXIII', 'IIIXII','IIIIXI','IIIIIX']
+#my_obs = ['YIIIII', 'IYIIII','IIYIII', 'IIIYII','IIIIYI','IIIIIY']
+#my_obs = ['ZIIIII', 'IZIIII','IIZIII', 'IIIZII','IIIIZI','IIIIIZ']
+
 #set the seed
 np.random.seed(123)
 algorithm_globals.random_seed = 123
 
 #load dataset with panda
 #data are scaled outside the notebook
-#sclaled_data_file = 'data/env.sel3.scaled.csv'
-data_file_csv = 'data/env.sel3.minmax.csv' 
-env = pd.read_csv(data_file_csv)  
+f_rate = 0.05 #rate of data sampling fot testing pourpose
+#data_file_csv = 'data/env.sel3.scaled.csv'
+data_file_csv = 'data/env.sel3.sk_sc.csv'
+env = pd.read_csv(data_file_csv).sample(frac=f_rate, random_state=seed)    
 
-
-#DEFINE design matrix
-
-f_rate = 1
-env_slice = env.sample(frac=f_rate, random_state=123) #slices the origin dataset
-
-Y = env_slice['occupancy']
-X = env_slice[['illuminance', 'blinds','lamps','rh', 'co2', 'temp']]
+Y = env['occupancy']
+X = env[['illuminance', 'blinds','lamps','rh', 'co2', 'temp']]
 
 #split design matrix (25% of the design matrix used for test)
 X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=123)
@@ -65,7 +70,6 @@ print(f'Max number of iteration used in kernel optimization: {max_iter}')
 print(f'Shape of dataset: {env.shape}')
 print(f'Training shape dataset {X_train.shape}')
 print(f'Label for traing {y_train.shape}')
-
 print(f'Test shape dataset {X_test.shape}')
 print(f'Label for test {y_test.shape}')
 
@@ -73,42 +77,36 @@ print(f'Label for test {y_test.shape}')
 NUM_QBIT = X_train.shape[1]
 
 #define the circuits
-encoding_circuit = Circuits.zzfeaturemap(n_wire=NUM_QBIT)
-trainable_circuit = Circuits.x_encoded(n_wire=NUM_QBIT, full_ent=False)
+encoding_circuit = Circuits.xyz_encoded(n_wire=NUM_QBIT)
+trainable_circuit = Circuits.zy_decomposition(param_prefix='tr', n_wire=NUM_QBIT, full_ent=False)
 encoding_circuit.barrier()
 fm = encoding_circuit.compose(trainable_circuit)
 training_params = trainable_circuit.parameters
+n_trainables = len(training_params)
 
 #show feature map
 print(f'*** TRAINABLE FEATURE MAP used in QSVC')
 print(fm.draw())
+print(f'Number of trainable paramenters: {n_trainables}')
 
 #define callback
 my_callback = QKCallback()
 
-#define the trainable kernel
-my_obs = ['ZIIIII', 'IZIIII','IIZIII', 'IIIZII','IIIIZI','IIIIIZ']
-nshots = 100 #paramenter using primitive estimator
+q_kernel = TrainablePQK_SVC(feature_map=fm, training_parameters=training_params, obs=my_obs, measure_fn=QMeasures.StateVectorEstimator, c_kernel=CKernels.rbf)
 
-measure_fn = QMeasures.StateVectorEstimator
-kernel = CKernels.linear
 
-#q_kernel = TrainableOuterQuantumKernel(feature_map=fm, training_parameters=training_params)
-q_kernel = TrainablePQK_SVC(feature_map=fm, training_parameters=training_params)
-q_kernel.configure(obs=my_obs, nshots=nshots, q_measure=QMeasures.StateVectorEstimator, c_kernel=CKernels.rbf)
 
 #print this info
-print(f'The QMeasure function used: {q_kernel.q_measure.__name__}')
-print(f'The classical kernel used: {q_kernel.kernel.__name__}')
+print(f'The QMeasure function used: {q_kernel.measure_fn.__name__}')
+print(f'The classical kernel used: {q_kernel.c_kernel.__name__}')
 print(f'The observables we use: {my_obs}')
-print(f'The numbers of shots (if applicable) for (qiskit) primitive estimator: {nshots}')
 
 #define updater, loss and inizial param
 spsa_opt = SPSA(maxiter=max_iter, learning_rate=0.03, perturbation=0.01, termination_checker=my_callback.callback)
 loss_func = SVCLoss(C=1.0)
 #init_point=[np.pi/2 for _ in range(NUM_QBIT)] #TODO: try random values (o pi)
 
-init_point = np.random.uniform(size=NUM_QBIT)
+init_point = np.random.uniform(size=n_trainables)
 print(f'Initial point: {init_point}')
 
 
@@ -138,6 +136,9 @@ score = accuracy_score(predictions, y_test)
 
 #final time (trainign + predict)
 jobs_final_time = time.time()
+
+#save the callback
+my_callback.save(prefix='TR_')
 
 print(f'*******SCORE: {score}')
 print(f'Time kernel training: {training_kernel_end - training_kernel_start} seconds.')
