376b4af32c6de69b1e2fecf6b9e503e850f2a91f
Francesco D'Amore <francesco.damore@gmail.com>
2024-12-09 17:04:24 +0100

    Removed circuit plot

--- jobs/run_QSVM_GS.py
+++ jobs/run_QSVM_GS.py
@@ -53,7 +53,6 @@ params_grid = {'C': [0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256, 512,
 #take a feature map and setting QSVC
 NUM_QBIT = 6 #commonly, this is the number of the features
 fm = Circuits.xyz_encoded(n_wire=NUM_QBIT, full_ent=False)   
-print(fm.draw())
 #fm = ZZFeatureMap(feature_dimension=NUM_QBIT)
 q_kernel = FidelityStatevectorKernel(feature_map=fm)
 svm_quantum = QSVC(quantum_kernel=q_kernel)
@@ -104,7 +103,21 @@ print(f'Accuracy Score on data: {score}')
 t_final = time.time()
 print(f'Time training: {t_training - t_start} seconds. Final time {t_final - t_start} seconds')
 
-# ***INFO RUN*** using XYZ with Ent
+# ***INFO RUN***
+# Name of Feature Map: XYZ
+#      ┌───────────┐┌───────────┐┌───────────┐                         ┌───┐
+# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├──■──────────────────────┤ X ├
+#      ├───────────┤├───────────┤├───────────┤┌─┴─┐                    └─┬─┘
+# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├┤ X ├──■───────────────────┼──
+#      ├───────────┤├───────────┤├───────────┤└───┘┌─┴─┐                 │  
+# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├─────┤ X ├──■──────────────┼──
+#      ├───────────┤├───────────┤├───────────┤     └───┘┌─┴─┐            │  
+# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├──────────┤ X ├──■─────────┼──
+#      ├───────────┤├───────────┤├───────────┤          └───┘┌─┴─┐       │  
+# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├───────────────┤ X ├──■────┼──
+#      ├───────────┤├───────────┤├───────────┤               └───┘┌─┴─┐  │  
+# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├────────────────────┤ X ├──■──
+#      └───────────┘└───────────┘└───────────┘                    └───┘     
 # N job param = 1
 # GridSearch Dict: {'C': [0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256, 512, 1024], 'gamma': array([0.01, 0.05, 0.75, 0.1 , 0.15, 0.25, 0.5 , 0.75, 1.  , 1.5 , 1.75,
 #        2.  , 2.5 ])}
@@ -126,49 +139,4 @@ print(f'Time training: {t_training - t_start} seconds. Final time {t_final - t_s
 # weighted avg       0.87      0.87      0.87       717
 
 # Accuracy Score on data: 0.8661087866108786
-
-# # ***INFO RUN*** USING XYZ no ENT
-# # N job param = 1
-# GridSearch Dict: {'C': [0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256, 512, 1024], 'gamma': array([0.01, 0.05, 0.75, 0.1 , 0.15, 0.25, 0.5 , 0.75, 1.  , 1.5 , 1.75,
-#        2.  , 2.5 ])}
-# Source file: data/env.sel3.sk_sc.csv
-# Shape of dataset: (2865, 7)
-# Training shape dataset (2148, 6)
-# Label for traing (2148,)
-# Test shape dataset (717, 6)
-# Label for test (717,)
-# Fitting 5 folds for each of 156 candidates, totalling 780 fits
-# Best paramenter: {'C': 1024, 'gamma': 0.01}
-#               precision    recall  f1-score   support
-
-#           -1       0.92      0.84      0.88       410
-#            1       0.81      0.91      0.85       307
-
-#     accuracy                           0.87       717
-#    macro avg       0.86      0.87      0.86       717
-# weighted avg       0.87      0.87      0.87       717
-
-# Accuracy Score on data: 0.8661087866108786
-
-# ***INFO RUN*** USING ZZFEATURE MAP
-# N job param = 1
-# GridSearch Dict: {'C': [0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256, 512, 1024], 'gamma': array([0.005, 0.01 , 0.05 , 0.75 , 0.1  , 0.15 , 0.25 , 0.5  , 0.75 ,
-#        1.   , 1.5  ])}
-# Source file: data/env.sel3.sk_sc.csv
-# Shape of dataset: (2865, 7)
-# Training shape dataset (2148, 6)
-# Label for traing (2148,)
-# Test shape dataset (717, 6)
-# Label for test (717,)
-# Fitting 5 folds for each of 132 candidates, totalling 660 fits
-# Best paramenter: {'C': 1.0, 'gamma': 0.005}
-#               precision    recall  f1-score   support
-
-#           -1       0.82      0.81      0.82       410
-#            1       0.75      0.76      0.76       307
-
-#     accuracy                           0.79       717
-#    macro avg       0.79      0.79      0.79       717
-# weighted avg       0.79      0.79      0.79       717
-
-# Accuracy Score on data: 0.7907949790794979
+# Time training: 10348.096881628036 seconds. Final time 10352.890522480011 seconds
