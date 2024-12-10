4a1668af510421b73c08e6f5a34ead01e5f0589a
Francesco D'Amore <francesco.damore@gmail.com>
2024-12-10 10:19:49 +0100

    Adding results no ent

--- jobs/run_QSVM_GS.py
+++ jobs/run_QSVM_GS.py
@@ -103,6 +103,47 @@ print(f'Accuracy Score on data: {score}')
 t_final = time.time()
 print(f'Time training: {t_training - t_start} seconds. Final time {t_final - t_start} seconds')
 
+
+# ***INFO RUN***
+# Name of Feature Map: XYZ
+#      ┌───────────┐┌───────────┐┌───────────┐
+# q_0: ┤ Rx(phi_0) ├┤ Ry(phi_0) ├┤ Rz(phi_0) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_1: ┤ Rx(phi_1) ├┤ Ry(phi_1) ├┤ Rz(phi_1) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_2: ┤ Rx(phi_2) ├┤ Ry(phi_2) ├┤ Rz(phi_2) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_3: ┤ Rx(phi_3) ├┤ Ry(phi_3) ├┤ Rz(phi_3) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_4: ┤ Rx(phi_4) ├┤ Ry(phi_4) ├┤ Rz(phi_4) ├
+#      ├───────────┤├───────────┤├───────────┤
+# q_5: ┤ Rx(phi_5) ├┤ Ry(phi_5) ├┤ Rz(phi_5) ├
+#      └───────────┘└───────────┘└───────────┘
+# N job param = 1
+# GridSearch Dict: {'C': [0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256, 512, 1024], 'gamma': array([0.01, 0.05, 0.75, 0.1 , 0.15, 0.25, 0.5 , 0.75, 1.  , 1.5 , 1.75,
+#        2.  , 2.5 ])}
+# Source file: data/env.sel3.sk_sc.csv
+# Shape of dataset: (2865, 7)
+# Training shape dataset (2148, 6)
+# Label for traing (2148,)
+# Test shape dataset (717, 6)
+# Label for test (717,)
+# Fitting 5 folds for each of 156 candidates, totalling 780 fits
+# Best paramenter: {'C': 1024, 'gamma': 0.01}
+#               precision    recall  f1-score   support
+
+#           -1       0.92      0.84      0.88       410
+#            1       0.81      0.91      0.85       307
+
+#     accuracy                           0.87       717
+#    macro avg       0.86      0.87      0.86       717
+# weighted avg       0.87      0.87      0.87       717
+
+# Accuracy Score on data: 0.8661087866108786
+# Time training: 14480.921637058258 seconds. Final time 14489.324667453766 seconds
+
+
+
 # ***INFO RUN***
 # Name of Feature Map: XYZ
 #      ┌───────────┐┌───────────┐┌───────────┐                         ┌───┐
