bae89e599496e361b1c671f2ca08897fffa8e55b
Francesco D'Amore <francesco.damore@gmail.com>
2025-03-03 18:36:14 +0100

    Fixing on SVM SMV_CLASSIC and related GS script

--- jobs/run_SVM_GS.py
+++ jobs/run_SVM_GS.py
@@ -18,10 +18,8 @@ seed=123              # the seed
 np.random.seed(seed)
 
 nfolds = 10 #set number of folds in CV
-f_rate = .05 #rate of data sampling fot testing pourpose
+f_rate = 1 #rate of data sampling fot testing pourpose
 nj = 1     # number of processors on the host machine. CAREFUL: it uses ALL PROCESSORS if n_jopbs = -1
-number_of_feature_copies = 1    # this is the number of times we stack the same identical feats (Annalisa's suggestion)
-
 
 #load dataset with panda
 #data are scaled outside the notebook
@@ -36,7 +34,7 @@ Y = env['occupancy']
 X = env[['illuminance', 'blinds','lamps','rh', 'co2', 'temp']]
 
 #WARNING: convert data to numpy. Quantum stuff (Qiskit) do not like PANDAS
-X_train_np = np.tile(X.to_numpy(), (1,number_of_feature_copies))  # to implement Annalisa's test
+X_train_np = X.to_numpy()
 y_train_np = Y.to_numpy()
 
 # defining a unique label for the simulation 
@@ -51,8 +49,8 @@ print(f'Label for traing {y_train_np.shape}')
 # define grid search strategy
 #Create a dictionary of possible parameters
 params_grid = {
-    'C': 2.**np.arange(1,12,2),
-    'gamma': 10**np.arange(-7,0.,2),
+    'C': [0.006, 0.015, 0.03, 0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256, 512, 1024],
+    'gamma': [0.25, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 20.0],
     'kernel':['rbf'] 
     }
 
@@ -105,55 +103,6 @@ with open(f'jobs/scores/accuracy' + id_string + '.txt', "w") as file:
     file.write(f'Shape of training dataset {X_train_np.shape}\n')
     file.write(f'Shape of training labels {y_train_np.shape}\n')
     file.write(f'Seed: {seed}\n')
-    file.write(f'Fitting {nfolds} folds for each of {len(ParameterGrid(grid.param_grid))} candidates, totalling 240 fits\n')
+    file.write(f'Fitting {nfolds} folds for each of {len(ParameterGrid(grid.param_grid))} candidates.\n')
     for i in range(nfolds):
         file.write(f"Fold {i+1}: {results[f'split{i}_test_score'][grid.best_index_]}\n")
-
-
-#Using a different scale method (standard sklearn)
-# Shape of dataset: (2865, 7)
-# Training shape dataset (2148, 6)
-# Label for traing (2148,)
-# Test shape dataset (717, 6)
-# Label for test (717,)
-# Fitting 5 folds for each of 270 candidates, totalling 1350 fits
-# Best paramenter: {'C': 16.0, 'gamma': 1.75, 'kernel': 'rbf'}
-# /home/francesco/git/QK/.venv/lib/python3.12/site-packages/sklearn/base.py:486: UserWarning: X has feature names, but SVC was fitted without feature names
-#   warnings.warn(
-#               precision    recall  f1-score   support
-
-#           -1       0.93      0.83      0.88       410
-#            1       0.80      0.92      0.86       307
-
-#     accuracy                           0.87       717
-#    macro avg       0.87      0.88      0.87       717
-# weighted avg       0.88      0.87      0.87       717
-
-# Accuracy Score on data: 0.8688981868898187
-
-# GridSearchCV 
-# Shape of dataset: (2865, 7)
-# Training shape dataset (2148, 6)
-# Label for traing (2148,)
-# Test shape dataset (717, 6)
-# Label for test (717,)
-# Fitting 5 folds for each of 234 candidates, totalling 1170 fits
-# Best paramenter: {'C': 8.0, 'gamma': 3.0, 'kernel': 'rbf'}
-# /home/francesco/git/QK/.venv/lib/python3.12/site-packages/sklearn/base.py:486: UserWarning: X has feature names, but SVC was fitted without feature names
-#   warnings.warn(
-#               precision    recall  f1-score   support
-
-#           -1       0.94      0.81      0.87       410
-#            1       0.79      0.93      0.86       307
-
-#     accuracy                           0.86       717
-#    macro avg       0.87      0.87      0.86       717
-# weighted avg       0.88      0.86      0.87       717
-# Accuracy Score on data: 0.8647140864714087
-
-#WRONG
-# Shape of dataset: (286, 7)
-# Training shape dataset (286, 6)
-# Label for traing (286,)
-# Best paramenter: {'C': 8.0, 'gamma': 0.25, 'kernel': 'rbf'}
-
