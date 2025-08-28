c701c5d8c5ef3897a07b44c32900d5cec5f48418
Francesco D'Amore <francesco.damore@gmail.com>
2025-08-28 18:06:22 +0200

    Adding info on number of support vectors to the output log.

--- jobs/run_SVM_GS.py
+++ jobs/run_SVM_GS.py
@@ -12,38 +12,6 @@ current_wd = os.getcwd()
 sys.path.append(current_wd)
 sys.path.append('./jobs/' + current_wd)
 
-#set the seed
-seed=123              # the seed
-np.random.seed(seed)
-
-nfolds = 10 #set number of folds in CV
-f_rate = 1 #rate of data sampling fot testing pourpose
-nj = 1     # number of processors on the host machine. CAREFUL: it uses ALL PROCESSORS if n_jopbs = -1
-
-#source_file = 'data/env.sel3.sk_sc.csv'
-#source_file = 'qfm/fm/qencoding/QC_3D_OBS_M1_ENT_FALSE.csv'
-#source_file = 'qfm/fm/qencoding/QC_X_OBS_M1_ENT_FALSE.csv'
-#source_file = 'qfm/fm/qencoding/QC_3D_OBS_M1_ENT_TRUE.csv'
-#source_file = 'qfm/fm/qencoding/QC_ZZ_OBS_XYZ.csv'
-#source_file = 'qfm/fm/qencoding/QC_IQP_OBS_XYZ.csv'
-#source_file = 'qfm/fm/qencoding/QC_TROTTER_OBS_XYZ.csv'
-#source_file = 'qfm/fm/qencoding/QC_X_OBS_M2_ENT_FALSE.csv'
-#source_file = 'qfm/fm/qencoding/QC_3D_OBS_M2_ENT_TRUE.csv'
-#source_file = 'qfm/fm/qencoding/QC_3D_OBS_M2_ENT_FALSE.csv'
-#source_file = 'qfm/fm/qencoding/QC_ZZ_OBS_M2.csv'
-#source_file = 'qfm/fm/qencoding/QC_IQP_OBS_M2.csv'
-#source_file = 'qfm/fm/qencoding/QC_TROTTER_OBS_M2.csv'
-#source_file = 'qfm/fm/qencoding/QC_X_OBS_MM_ENT_FALSE.csv'
-#source_file = 'qfm/fm/qencoding/QC_3D_OBS_MM_ENT_TRUE.csv'
-#source_file = 'qfm/fm/qencoding/QC_3D_OBS_MM_ENT_FALSE.csv'
-#source_file = 'qfm/fm/qencoding/QC_ZZ_OBS_MM.csv'
-#source_file = 'qfm/fm/qencoding/QC_IQP_OBS_MM.csv'
-source_file = 'qfm/fm/qencoding/QC_3D_OBS_M2_L2.csv'
-
-
-
-
-
 def get_origin_data(f_rate, source_file) -> tuple[np.ndarray, np.ndarray, pd.DataFrame]:
 
     #load dataset with panda    
@@ -76,9 +44,42 @@ def get_qencoded_data(f_rate, source_file) -> tuple[np.ndarray, np.ndarray, pd.D
 
     return (X_train_np,y_train_np,env)
 
+
+
+#set the seed
+seed=123              # the seed
+np.random.seed(seed)
+
+nfolds = 10 #set number of folds in CV
+f_rate = 0.2 #rate of data sampling fot testing pourpose
+nj = 1     # number of processors on the host machine. CAREFUL: it uses ALL PROCESSORS if n_jopbs = -1
+eval_score = 'accuracy'  #evaluation score for the grid search
+
+source_file = 'data/env.sel3.sk_sc.csv'
+#source_file = 'qfm/fm/qencoding/QC_3D_OBS_M1_ENT_FALSE.csv'
+#source_file = 'qfm/fm/qencoding/QC_X_OBS_M1_ENT_FALSE.csv'
+#source_file = 'qfm/fm/qencoding/QC_3D_OBS_M1_ENT_TRUE.csv'
+#source_file = 'qfm/fm/qencoding/QC_ZZ_OBS_XYZ.csv'
+#source_file = 'qfm/fm/qencoding/QC_IQP_OBS_XYZ.csv'
+#source_file = 'qfm/fm/qencoding/QC_TROTTER_OBS_XYZ.csv'
+#source_file = 'qfm/fm/qencoding/QC_X_OBS_M2_ENT_FALSE.csv'
+#source_file = 'qfm/fm/qencoding/QC_3D_OBS_M2_ENT_TRUE.csv'
+#source_file = 'qfm/fm/qencoding/QC_3D_OBS_M2_ENT_FALSE.csv'
+#source_file = 'qfm/fm/qencoding/QC_ZZ_OBS_M2.csv'
+#source_file = 'qfm/fm/qencoding/QC_IQP_OBS_M2.csv'
+#source_file = 'qfm/fm/qencoding/QC_TROTTER_OBS_M2.csv'
+#source_file = 'qfm/fm/qencoding/QC_X_OBS_MM_ENT_FALSE.csv'
+#source_file = 'qfm/fm/qencoding/QC_3D_OBS_MM_ENT_TRUE.csv'
+#source_file = 'qfm/fm/qencoding/QC_3D_OBS_MM_ENT_FALSE.csv'
+#source_file = 'qfm/fm/qencoding/QC_ZZ_OBS_MM.csv'
+#source_file = 'qfm/fm/qencoding/QC_IQP_OBS_MM.csv'
+#source_file = 'qfm/fm/qencoding/QC_3D_OBS_M2_L2.csv'
+
+
+
 #get origin data
-#X_train_np,y_train_np,env = get_origin_data(f_rate, source_file)
-X_train_np,y_train_np,env = get_qencoded_data(f_rate, source_file)
+X_train_np,y_train_np,env = get_origin_data(f_rate, source_file)
+#X_train_np,y_train_np,env = get_qencoded_data(f_rate, source_file)
 
 # defining a unique label for the simulation 
 id_string = f'_SVM_{X_train_np.shape[1]}feats_{nfolds}folds_seed{seed}_frate{f_rate}'
@@ -100,7 +101,8 @@ params_grid = {
     }
 
 #Create the GridSearchCV object
-grid = GridSearchCV(SVC(), params_grid, verbose=1, cv=nfolds, n_jobs=nj)
+grid = GridSearchCV(SVC(), params_grid, verbose=1, cv=nfolds, n_jobs=nj, scoring=eval_score)
+
 
 #get time
 t_start = time.time()
@@ -111,11 +113,15 @@ grid_clf = grid.fit(X_train_np, y_train_np)
 #get time training
 t_training = time.time()
 
+num_support_vectors = grid.best_estimator_.n_support_
+
 #Print the best estimator with it's parameters
+print(f'Used score: {grid.scoring}')
 print(f'Best paramenter: {grid.best_params_}')
 print(f'Time training: {t_training - t_start} seconds')
 print(f'Best score {grid.best_score_}')
 print(f'Results: {grid.cv_results_.keys()}')
+print(f'Number of SV of the best model: {num_support_vectors}')
 
 # taking the largest average accuracy of the grid search and the corresponding standard dev.
 cv_mean = grid.cv_results_['mean_test_score'][grid.best_index_]
@@ -125,21 +131,23 @@ results = grid.cv_results_
 for i in range(nfolds):
     print(f"Fold {i+1}: {results[f'split{i}_test_score'][grid.best_index_]}")
 
-print(f'\nAverage accuracy, best score: {cv_mean:.6f}')
-print(f'Standard deviation, best score: {cv_std:.6f}')
+print(f'\nBest score (Average): {cv_mean:.6f}')
+print(f'Best score (Standard Deviation): {cv_std:.6f}')
 
 print(f'{t_training-t_start} seconds elapsed.')
 
 # the confidence interval is given by:   mean +/- 2 * stdev / sqrt(N)
-final_msg = f'Accuracy (95% confidence) = {cv_mean:.6f} +/- {2*cv_std/np.sqrt(nfolds):.6f} == [{cv_mean - 2*cv_std/np.sqrt(nfolds):.6f}, {cv_mean + 2*cv_std/np.sqrt(nfolds):.6f}]'
+final_msg = f'Score (95% confidence) = {cv_mean:.6f} +/- {2*cv_std/np.sqrt(nfolds):.6f} == [{cv_mean - 2*cv_std/np.sqrt(nfolds):.6f}, {cv_mean + 2*cv_std/np.sqrt(nfolds):.6f}]'
 print(final_msg)
 
 # INFORMATION SAVED IN THE 'accuracy*.txt' OUTPUT FILES
-with open(f'accuracy' + id_string + '.txt', "w+") as file:
+with open(f'accuracy' + id_string + '.txt', "w+") as file:    
     file.write(final_msg + '\n\n')
     file.write(datetime.today().strftime('%Y-%m-%d %H:%M:%S') + '\n')
     file.write(f'{t_training-t_start:.1f} seconds elapsed.\n')
+    file.write(f'Used score: {grid.scoring}\n')
     file.write(f'Best parameter: {grid.best_params_}\n')
+    file.write(f'Number of SV of the best model: {num_support_vectors}\n')
     file.write(f'N job param = {nj}\n')
     file.write(f'GridSearch Dict: {params_grid}\n')
     #check the shape of test and training dataset
